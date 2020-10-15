8a510e605d918fcde3f2fbfa854ee62b7dd6778e
Jimmy Svensson <jimmy888swe@gmail.com>
2020-10-15 22:26:37 +0200

    feat: fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Events from './Components/Events';
 
 function App() {
+  // state 
+  const [events, setEvents] = useState([]);
+
+  // useEffect to fetch event data
+  useEffect(() => {   
+    const fetchData = async () => {
+        const response = await fetch('https://crudcrud.com/api/c974ef5540f34583aacee5422b10fc66/event-list'); 
+        const data = await response.json();
+        setEvents(data);
+        console.log(data);
+      }
+      fetchData();
+  }, []);
+
+  
+  
   return (
     <div className="App">
-   <p> Place code here!</p>
+      <Events event={events} />
     </div>
   );
 }
---
+++ src/Components/Events.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Events(event) {
+
+  console.log(event.event.id);
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
