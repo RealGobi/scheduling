e7b776de94b5df794f2d618c62112372be9851bb
Jimmy Svensson <jimmy888swe@gmail.com>
2020-10-19 17:03:57 +0200

    feat: POST new meeting

--- src/Components/AddMeeting.js
+++ src/Components/AddMeeting.js
@@ -3,7 +3,26 @@ import './addMeeting.css';
 
 export default function AddMeeting() {
 
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('https://crudcrud.com/api/c974ef5540f34583aacee5422b10fc66/event-list', {
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    method: 'POST',
+    body: JSON.stringify({
+      location,
+      activity,
+      startDate,
+      endDate
+    })
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  setLocation('');
+  setActivity('');
+  setStartDate('');
+  setEndDate('');
+  }
   return (
     <div className='add-meeting-box'>
       
