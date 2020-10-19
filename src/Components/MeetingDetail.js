5c5b3ee1668abf3fdc9a811032e0854ffdd8085d
Jimmy Svensson <jimmy888swe@gmail.com>
2020-10-19 17:05:46 +0200

    feat: delete with history push for now

--- src/Components/MeetingDetail.js
+++ src/Components/MeetingDetail.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect} from 'react';
+import { useHistory } from "react-router-dom";
+
 import PropTypes from 'prop-types';
+import { Button } from '@material-ui/core';
+
 
 import './meetingDetail.css';
 
@@ -8,7 +12,8 @@ export default function MeetingDetail({ match }) {
      // state 
      const [meetingDetails, setMeetingDeails] = useState({});
      const url = `https://crudcrud.com/api/c974ef5540f34583aacee5422b10fc66/event-list/${match.params.id}`;
-   
+     const history = useHistory();
+
      // useEffect to fetch event data
      useEffect(() => {   
        fetchData();
@@ -21,6 +26,19 @@ export default function MeetingDetail({ match }) {
           
           setMeetingDeails(data);
         }
+
+        // delete
+const deleteClick = (id) => {
+  fetch(`https://crudcrud.com/api/c974ef5540f34583aacee5422b10fc66/event-list/${id}`, {
+      method: 'DELETE'
+    })
+    .then(response => console.log(response))
+    history.push("/")
+  }
+
+const btnStyle = {
+  width: '100%'
+}
         
   return (
     <div className="meetingDetail">
@@ -29,6 +47,7 @@ export default function MeetingDetail({ match }) {
         <p>Aktivitet: {meetingDetails.activity}</p>
         <p>Datum och tid: {meetingDetails.startDate} - {meetingDetails.endDate}</p>
       </div>
+      <Button onClick={() => deleteClick(meetingDetails._id)} style={btnStyle} variant="contained" color="secondary">Delete</Button>
     </div>
   )
 }
