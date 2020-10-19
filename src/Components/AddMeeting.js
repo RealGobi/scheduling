f079b8ec820d3a59adf18d986baa544d59350c38
Jimmy Svensson <jimmy888swe@gmail.com>
2020-10-19 17:05:10 +0200

    feat: form with material ui button

--- src/Components/AddMeeting.js
+++ src/Components/AddMeeting.js
@@ -31,7 +31,39 @@ export default function AddMeeting() {
   }
   return (
     <div className='add-meeting-box'>
-      
+      <form onSubmit={handleSubmit}>
+      <div className="form-group">
+          <label>Plats: </label>
+          <input type="text"
+                 className="form-control"
+                 value={location}
+                 onChange={e => setLocation(e.target.value)} />
+        </div>
+      <div className="form-group">
+          <label>Aktivitet: </label>
+          <input type="text"
+                 className="form-control"
+                 value={activity}
+                 onChange={e => setActivity(e.target.value)} />
+        </div>
+      <div className="form-group">
+          <label>Startdatum och tid: </label>
+           <p className='date-info-text'>(YYYY-MM-DD HH:MM)</p>
+          <input type="text"
+                 className="form-control"
+                 value={startDate}
+                 onChange={e => setStartDate(e.target.value)} />
+        </div>
+      <div className="form-group">
+          <label>Slutdatum och tid: </label>
+           <p className='date-info-text'>(YYYY-MM-DD HH:MM)</p>
+          <input type="text"
+                 className="form-control"
+                 value={endDate}
+                 onChange={e => setEndDate(e.target.value)} />
+        </div>
+        <Button id='btn-submit' variant="contained" color="primary" type="submit" value="Submit" >Add meeting</Button>
+      </form>
     </div>
   )
 }
