3023a0c533f1dcd5ceab239a810ff467ee68252e
Jimmy Svensson <jimmy888swe@gmail.com>
2020-10-20 16:46:44 +0200

    style: customize material ui

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#d9b96f',
+    },
+    secondary: {
+      main: '#de4b4b',
+    },
+  },
+});
+
+export default theme;
