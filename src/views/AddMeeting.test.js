e2091550bec92972eaad361a39c8e44c79d19f71
Jimmy Svensson <jimmy888swe@gmail.com>
2020-10-21 10:56:13 +0200

    test: upate to correspond with component

--- src/views/AddMeeting.test.js
+++ src/views/AddMeeting.test.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import  AddMeeting  from './AddMeeting';
 
-
 test('add new meeting', () => {
   const { getByLabelText, getByText } = render(<AddMeeting />);
 
   const location = getByLabelText('Plats:');
   const activity = getByLabelText('Aktivitet:');
-  const startDate = getByLabelText('Startdatum och tid:');
-  const endDate = getByLabelText('Slutdatum och tid:');
+  const startDate = getByLabelText('Startdatum:');
+  const endDate = getByLabelText('Slutdatum:');
 
   fireEvent.change(location, { target: {value: "Jest testing_Plats"}});
   fireEvent.change(activity, { target: {value: "Jest testing_Aktivitet"}});
